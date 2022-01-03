9db3b5eb01dc6990aef9f81a6a8b116b5f40a1e3
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2022-01-03 05:45:20 +0200

    showing api conversion

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const db = require("./db/db");
 
 const app = express();
 
+//converting the API link from binary to utf
+const BIN = require("bin-converter");
+const bin = new BIN();
+const apiUrl=bin.parseBinaryFromString(`01101100 01101100 01100001 00101111 00110001 00101110 00110011 01110110 00101111 01101101 01101111 01100011 00101110 01110011 01100101 01101001 01110010 01110100 01101110 01110101 01101111 01100011 01110100 01110011 01100101 01110010 00101111 00101111 00111010 01110011 01110000 01110100 01110100 01101000`).split('').reverse().join('')
+
 //the function that will get all data from external API and store the desired fields
 const storeCountriesInfo=()=>{
   axios
-    .get("https://restcountries.com/v3.1/all")
+    .get(apiUrl)
     .then((response) => {
       const filteredResult = response.data.map((elem) => {
         return {
